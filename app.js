233b94a15127c2630772939447a002f8437f46f8
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-03-07 21:25:24 +0530

    Understood redirecting requests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
@@ -6,12 +7,19 @@ const server = http.createServer((req, res) => {
     // req.method -> return the method of the url like GET, POST
     // req.url -> returns everything after the host address(localhost:3000), if you add test in the url it returns /test
     const url = req.url;
+    const method = req.method;
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
