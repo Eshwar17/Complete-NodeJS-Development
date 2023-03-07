ffb02a47ec78c2fa7c28e201a7a8ef6c3b26d54d
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-03-07 19:33:18 +0530

    Understood request

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // req.header -> returns all the headers in the url
+    // req.method -> return the method of the url like GET, POST
+    // req.url -> returns everything after the host address(localhost:3000), if you add test in the url it returns /test
 });
 
 server.listen(3000);
